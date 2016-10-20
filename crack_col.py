4ac72aca61477337fccf290c6baafdf8bf05bd13
Smeths <johnsmethurst@gmail.com>
2016-10-20 13:19:21 -0400

    updated col crack

--- crack_col.py
+++ crack_col.py
@@ -18,15 +18,19 @@ col = ColTrans_test(cipher=cipher)
 
 quadgrams = ngram("quadgrams.txt")
 bestscore = -1000000
+alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
+         "P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-for perm in permutations(["A","B","C","D","E"],5):
-    test_key = ''.join(list(perm))
-    decipher = col.decipher(test_key)
-    score = quadgrams.run(decipher)
-    if score > bestscore:
-       deciphered = decipher
-       key = test_key
-       bestscore = score
+for i in range(1,7):
+    print alpha[0:i],i
+    for perm in permutations(alpha[0:i],i):
+        test_key = ''.join(list(perm))
+        decipher = col.decipher(test_key)
+        score = quadgrams.run(decipher)
+        if score > bestscore:
+            deciphered = decipher
+            key = test_key
+            bestscore = score
 
 print deciphered
 print key
